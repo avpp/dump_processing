23f99bf8bac6efd6f66fd635f2b846b4307462ea
Alexey <nuzhaleks2@rambler.ru>
2013-10-14 20:13:33 +0400

    add errors files, add google dns servers

--- firmware/files/clt.sh
+++ firmware/files/clt.sh
@@ -1,2 +1,2 @@
 #!/bin/ash
-tcpdump -n -e -tt -vvv -i mon.wlan0 type mgt subtype probe-req 2>>/tcpdump.errors | /processing.exe 2>>/pprocessing.errors
+tcpdump -n -e -tt -vvv -i mon.wlan0 type mgt subtype probe-req 2>>/tcpdump.errors | /processing.exe 2>>/processing.errors
