2d07a3e56eb9063f4db262d2a9d3623554b05529
Alexey <nuzhaleks2@rambler.ru>
2013-10-22 13:36:44 +0400

    fix create problem

--- firmware/files/create.sh
+++ firmware/files/create.sh
@@ -3,7 +3,7 @@ RESULT="FALSE"
 while [ $RESULT == "FALSE" ]; do
   iw phy phy0 interface add mon.wlan0 type monitor
   VALUE=$( ifconfig mon.wlan0 up | grep "No such device" )
-  if [ $VALUE == "" ]; then
+  if [ "$VALUE" == "" ]; then
     RESULT="TRUE"
   else
     echo -e "[$(date)]Can't create or up mon.wlan0 interface." >> boot.errors
